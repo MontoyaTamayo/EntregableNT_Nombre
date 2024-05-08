8034f8463612e9a6d80529ca092db532853b6aed
MontoyaTamayo <160282661+MontoyaTamayo@users.noreply.github.com>
2024-05-08 16:57:18 -0500

    Add files via upload

--- Python_projects/Entregable2.py
+++ Python_projects/Entregable2.py
@@ -1,1 +1,17 @@
+#Repetición entregable dedido a que se borro
+usuario = input("Ingrese su nombre de usuario: ")
+contraseña = input("Ingrese su contraseña: ")
+telefono = input("Ingrese su número de teléfono: ")
 
+# Supongamos solo que tienes un usuario permitido
+usuario_permitido = ["Am@mail.com"]
+contraseña = {"1208"}
+telefono = {"3196320618"}
+
+if usuario in usuario_permitido:
+    if contraseña == contraseña[1208] and telefono == telefono[319632618]:
+        print("Inicio de sesión exitoso")
+    else:
+        print("La contraseña o el número de teléfono es incorrecto")
+else:
+    print("El usuario no está permitido")
